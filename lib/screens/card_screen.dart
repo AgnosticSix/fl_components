f44f172d81257cbdb6119f6371cb4d28f70f0926
MATEO JAVIER AGUILAR CARRILLO <mateo.aguilar@elektra.com.mx>
2022-07-04 12:50:02 -0500

    url of image deleted

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -22,11 +22,6 @@ class CardScreen extends StatelessWidget {
             ),
             SizedBox(height: 10),
             CustomCardType2(
-                cardText: 'Paisaje 2',
-                imageUrl:
-                    'https://scontent.fmex28-1.fna.fbcdn.net/v/t31.18172-8/14859928_1431664593530190_7165823284348636226_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeFLVsqxYNnPk1sTLq-g7qYcYLjiIJ57kFlguOIgnnuQWT8e9837rHyb4DE7YA5XKoon2AAkVtlbrZTPg-OJCnID&_nc_ohc=GCYuYGRWftUAX-6ndT3&_nc_ht=scontent.fmex28-1.fna&oh=00_AT_fPAlAnpSK98Tzg6XSX110aNPe8HSpKC75H7Z32JQENg&oe=62E7E617'),
-            SizedBox(height: 10),
-            CustomCardType2(
                 imageUrl:
                     'https://img.freepik.com/vector-gratis/paisaje-montana-diseno-plano_23-2149161403.jpg?w=2000'),
             SizedBox(height: 10),
