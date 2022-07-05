72aa24208339873c6d9a76b57eaeddac1b5e905b
MATEO JAVIER AGUILAR CARRILLO <mateo.aguilar@elektra.com.mx>
2022-07-05 11:46:44 -0500

    animated container added

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -12,8 +12,8 @@ class AlertScreen extends StatelessWidget {
         context: context,
         builder: (context) {
           return CupertinoAlertDialog(
-            title: Text('Titulo'),
-            content: Column(mainAxisSize: MainAxisSize.min, children: [
+            title: const Text('Titulo'),
+            content: Column(mainAxisSize: MainAxisSize.min, children: const [
               Text("Este es el contenido de la alerta"),
               SizedBox(
                 height: 10,
@@ -45,8 +45,8 @@ class AlertScreen extends StatelessWidget {
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadiusDirectional.circular(15)),
             elevation: 5,
-            title: Text('Titulo'),
-            content: Column(mainAxisSize: MainAxisSize.min, children: [
+            title: const Text('Titulo'),
+            content: Column(mainAxisSize: MainAxisSize.min, children: const [
               Text("Este es el contenido de la alerta"),
               SizedBox(
                 height: 10,
