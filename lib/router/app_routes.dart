5e21c15b7ffc3e26f60210af6c5b3278a55279d1
MATEO JAVIER AGUILAR CARRILLO <mateo.aguilar@elektra.com.mx>
2022-07-05 13:15:10 -0500

    text inputs screen added

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -6,12 +6,6 @@ class AppRoutes {
   static const initialRoute = 'home';
 
   static final menuOptions = <MenuOption>[
-    //TODO: borrar home
-    MenuOption(
-        route: 'home',
-        name: 'Home Screen',
-        screen: const HomeScreen(),
-        icon: Icons.home),
     MenuOption(
         route: 'listview1',
         name: 'List View 1',
@@ -42,10 +36,17 @@ class AppRoutes {
         name: 'Animated Container',
         screen: const AnimatedScreen(),
         icon: Icons.play_arrow_outlined),
+    MenuOption(
+        route: 'inputs',
+        name: 'Text Inputs',
+        screen: const InputsScreen(),
+        icon: Icons.input_rounded),
   ];
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes() {
     Map<String, Widget Function(BuildContext)> appRoutes = {};
+
+    appRoutes.addAll({'home': (BuildContext context) => const HomeScreen()});
 
     for (final option in menuOptions) {
       appRoutes.addAll({option.route: (BuildContext context) => option.screen});
